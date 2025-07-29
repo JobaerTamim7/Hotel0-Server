83dee1cdbfc854be7a4162fb3c1d284a020d9a81
Tamim <jobaertamim7@gmail.com>
2025-07-29 21:30:04 +0600

    feat(room): add room by id

--- src/Room/getRoom.js
+++ src/Room/getRoom.js
@@ -15,4 +15,22 @@ RoomRouter.get("/all", async (req, res) => {
   }
 });
 
+RoomRouter.get("/:id", async (req, res) => {
+  const roomId = req.params.id;
+  try {
+    const roomCollection = await getCollection("rooms");
+    const room = await roomCollection.findOne({
+      roomId: roomId,
+    });
+    if (!room) {
+      return res.status(404).json({ error: "Room not found" });
+    }
+    console.log("Fetched room:", room);
+    res.status(200).json(room);
+  } catch (error) {
+    console.error("Error fetching room:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 export default RoomRouter;
